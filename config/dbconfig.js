5fefea94cccbc218b306996794f4d1778ca06baa
Guilherme Augusto Gomes Cunha <41303580+CunhaGuilhermeBR@users.noreply.github.com>
2021-05-25 22:17:40 -0300

    dev

--- config/dbconfig.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    secret: 'yoursecret',
-    database: 'mongodb+srv://Guilherme:senha12345@leiloncluster.72nrw.mongodb.net/leilonDb?retryWrites=true&w=majority'
-}
--- views/images/generateImg.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs')
-
-    saveInDrive :// Função que gera o arquivo baseado na String Base64 que é passada, gera o arquivo e o salva no drive. Retorna o link
- var functions = {
-    
- }   
- 
-
-         module.exports = functions
